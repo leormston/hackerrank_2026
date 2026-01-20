8d6addc276bae692f19137235819e2663531b3b4
leormston <l.e.ormston@gmail.com>
2026-01-20 23:48:15 +0000

    plus_minus

---
+++ data_structures/easy/plus_minus.py
@@ -0,0 +1,38 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'plusMinus' function below.
+#
+# The function accepts INTEGER_ARRAY arr as parameter.
+#
+
+def plusMinus(arr):
+    # Write your code here
+    positive = 0
+    negative = 0
+    zero = 0
+    length = len(arr)
+    for i in arr:
+        if i < 0:
+            negative += 1
+        elif i > 0:
+            positive += 1
+        else:
+            zero += 1
+    
+    result = [format(positive / length, ".6f"), format(negative / length, ".6f"), format(zero / length, ".6f")]
+    
+    for i in result:
+        print(i)
+if __name__ == '__main__':
+    n = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    plusMinus(arr)
